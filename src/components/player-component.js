232a4a71ead44b80ead3316d303f0ccaeee2a1c6
emmanuel <ejariza@gmail.com>
2020-05-21 23:24:25 -0500

    Playback bpm are modifiable by user in PlayerComponent

--- src/components/player-component.js
+++ src/components/player-component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, InputGroup, FormControl } from 'react-bootstrap';
 import * as mm from '@magenta/music';
 import { saveAs } from 'file-saver';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
@@ -9,7 +9,7 @@ class PlayerComponent extends React.Component {
   constructor(props){
     super(props);
     this.player = new mm.SoundFontPlayer(props.soundfontUrl);
-    this.state = { sequence: null }
+    this.state = { sequence: null, bpm: 90 }
   }
 
   updateSequence(sequence){
@@ -18,24 +18,48 @@ class PlayerComponent extends React.Component {
     })
   }
 
+  updateBpm(bpm){
+    this.setState({
+      bpm: bpm,
+    })
+  }
+
   render(){
     const play = () => {
-      this.player.start(this.state.sequence, 90);
+      this.player.start(this.state.sequence, this.state.bpm);
     }
 
     const download = () => {
         saveAs(new File([mm.sequenceProtoToMidi(this.state.sequence)], 'seq.mid'));
     }
 
-    return (<div>
+    const updateBmp = this.updateBpm.bind(this);
+
+    return (<div className="content">
               {this.state.sequence?(
-                <div>
-                 <Button variant="outline-primary" onClick = { play }>
-                    <PlayArrowIcon/>
-                 </Button>
-                 <Button variant="outline-primary" onClick = { download }>
-                    <CloudDownloadRoundedIcon/>
-                 </Button>
+                <div className="row justify-content-center">
+                   <InputGroup className="col-2" >
+                     <InputGroup.Prepend>
+                       <InputGroup.Text id="bpm-label">bpm:</InputGroup.Text>
+                     </InputGroup.Prepend>
+                     <FormControl
+                       placeholder=""
+                       value= { `${this.state.bpm}` }
+                       aria-describedby="bpm-label"
+                       type="number"
+                       onChange= { event => updateBmp( event.target.value ) }
+                     />
+                   </InputGroup>
+                   <div className="col-1" >
+                     <Button variant="outline-primary" onClick = { play }>
+                        <PlayArrowIcon/>
+                     </Button>
+                   </div>
+                   <div className="col-1">
+                     <Button variant="outline-primary" onClick = { download }>
+                        <CloudDownloadRoundedIcon/>
+                     </Button>
+                   </div>
                 </div>
               ):""}
            </div>)
